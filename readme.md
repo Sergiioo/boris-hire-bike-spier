a080aaf25ae8f206b27edbe3c6cd81bcffec0d54
Sergio Annecchiarico <sergio@serg.io>
2016-07-05 22:21:16 +0100

    initial commit of basic functionality

---
+++ readme.md
@@ -0,0 +1,13 @@
+dockable
+========
+
+Nothing to do with docker!
+
+Boris bikes are great, accept for one problem. Parking.
+
+You get off the train.
+You hop on a Boris bike.
+You cycle to work.
+You may or may not be able to park near your office!
+
+This is 
---
+++ src/js/dockable.js
@@ -0,0 +1,69 @@
+var dockable = function dockable() {
+
+    function isDockable(bikePoint, appId, appKey) {
+
+        var request = new Request('https://api.tfl.gov.uk/BikePoint/' + bikePoint + '?app_id=' + appId + '&app_key=' + appKey);
+        fetch(request).then(function (response) {
+
+            response.json().then(function (tflResponse) {
+
+                document.getElementById('station').innerHTML = tflResponse.commonName;;
+
+                if (tflResponse.additionalProperties && Array.isArray(tflResponse.additionalProperties)) {
+                    var bikeCount = tflResponse.additionalProperties.find(function (properties) {
+                        return properties && properties.key && properties.key === 'NbBikes';
+                    });
+                    var spaceCount = tflResponse.additionalProperties.find(function (properties) {
+                        return properties && properties.key && properties.key === 'NbEmptyDocks';
+                    });
+                    var dockCount = tflResponse.additionalProperties.find(function (properties) {
+                        return properties && properties.key && properties.key === "NbDocks";
+                    });
+
+                    showResults(bikeCount.value, spaceCount.value);
+
+                    console.log("bike count: " + bikeCount.value + ", space count: " + spaceCount.value + ", total docks: " + dockCount.value);
+                }
+            })
+        });
+    }
+
+    function showResults(bikeCount, spaceCount) {
+        var bikesElement = document.getElementById('bikes');
+        bikesElement.innerHTML = bikeCount;
+
+        var spacesElement = document.getElementById('spaces');
+        spacesElement.innerHTML = spaceCount;
+    }
+
+    function reset() {
+        document.getElementById('station').innerHTML = '';
+        document.getElementById('bikes').innerHTML = '';
+        document.getElementById('spaces').innerHTML = '';
+    }
+
+    function refresh() {
+        reset();
+        isDockable("BikePoints_340", getAppId(), getAppKey());
+    }
+
+    function getAppId() {
+        return '';
+    }
+
+    function getAppKey() {
+        return '';
+    }
+
+    return {
+        isDockable: isDockable,
+        getAppId: getAppId,
+        getAppKey: getAppKey,
+        refresh: refresh,
+        reset: reset
+    }
+
+}();
+
+// document.body.requestFullscreen();
+dockable.refresh();
